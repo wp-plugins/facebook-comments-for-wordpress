6653f9fa05604606eb970758afb82ef849113133
we8u <we8u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-08 14:17:20 +0000

    Beta version fixing dashboard jQuery conflict and conflict with global var $options (renamed to $fbc_options)
    git-svn-id: https://plugins.svn.wordpress.org/facebook-comments-for-wordpress/tags/3.0.2.2-beta@357211 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebook-comments-core.php
@@ -0,0 +1,194 @@
+<?php
+/**********************************
+Common functions
+**********************************/
+
+// Update database with default options upon plugin activation
+function fbComments_init() {
+	global $fbComments_defaults, $fbc_options;
+	
+	add_option('fbComments', $fbComments_defaults); // on first install
+	
+	$oldver = get_option('fbComments_ver');
+	if (empty($oldver) || $oldver != FBCOMMENTS_VER) {
+		fbComments_doUpdate();
+	}
+	update_option('fbComments_ver', FBCOMMENTS_VER);
+	
+	
+	// If the plugin has been activated before and we already have the integral settings, cache all Facebook comment counts
+	if (!empty($fbc_options['appId']) &&
+		!empty($fbc_options['appSecret'])) {
+			update_option('fbComments_displayAppIdWarning', false);
+			fbComments_cacheAllCommentCounts();
+	} else { update_option('fbComments_displayAppIdWarning', true); }
+}
+
+ /**
+ * Load settings from database on update.
+ *
+ * 3.1 changed activation hook firing on update. see: http://bit.ly/wp3_1noupdate
+ *
+ * @since 3.0.2
+ */
+function fbComments_doUpdate() {
+	global $fbComments_defaults, $fbc_options;
+	
+	if (($_options = get_option('fbComments')) != false) $fbc_options = $_options;
+	add_option('fbComments', $fbComments_defaults); //copy defaults to db
+	
+	$save_appId = $fbc_options['appId'];			// save id, secret, xid in case upgrading 
+	$save_appSecret = $fbc_options['appSecret'];
+	$save_xid = $fbc_options['xid'];
+	
+	$_appId = get_option('fbComments_appId');
+	$_appSecret = get_option('fbComments_appSecret');
+	if (strlen($_appId) > 1) $fbc_options['appId'] = $_appId; 
+	else 					 $fbc_options['appId'] = $save_appId;
+	
+	if (strlen($_appSecret) > 1) $fbc_options['appSecret'] = $_appSecret;
+	else 						 $fbc_options['appSecret'] = $save_appSecret;
+	
+	$fbc_options['xid'] = get_option('fbComments_xid');	// see if upgrading from v < 3
+	if (strlen($fbc_options['xid']) < 1) { $fbc_options['xid'] = $save_xid; }
+	
+	$fbc_options['includeFbJs'] = true;
+	$fbc_options['includeFbJsOldWay'] = false;
+	$fbc_options['includeFbmlLangAttr'] = true;
+	$fbc_options['includeOpenGraphLangAttr'] = true;
+	$fbc_options['includeOpenGraphMeta'] = true;
+	$fbc_options['language'] = get_option('fbComments_language');
+	$fbc_options['dashNumComments'] = 10;
+	
+	// handle upgrading from <=2.1.2
+	if (get_option('fbComments_includeFbComments') == true) { $fbc_options['includeFbComments'] = true; }
+	if (get_option('fbComments_hideWpComments') == true) { $fbc_options['hideWpComments'] = true; } 
+	if (get_option('fbComments_combineCommentCounts') == true) { $fbc_options['combineCommentCounts'] = true; }
+	if (get_option('fbComments_notify') == true) { $fbc_options['notify'] = true; }
+	if (get_option('fbComments_displayTitle') == true) { $fbc_options['displayTitle'] = true; }
+	if (get_option('fbComments_publishToWall') == true) { $fbc_options['publishToWall'] = true; }
+	if (get_option('fbComments_reverseOrder') == true) { $fbc_options['reverseOrder'] = true; }
+	if (get_option('fbComments_hideFbLikeButton') == true) { $fbc_options['hideFbLikeButton'] = true; }
+	if (get_option('fbComments_darkSite') == true) { $fbc_options['darkSite'] = true; }
+	if (get_option('fbComments_noBox') == true) { $fbc_options['noBox'] = true; }
+	if (strlen($_title = get_option('fbComments_title')) > 1) $fbc_options['title'] = $_title;
+	if (strlen($_numPosts = get_option('fbComments_numPosts')) > 1) $fbc_options['numPosts'] = $_numPosts;
+	if (strlen($_width = get_option('fbComments_width')) > 1) $fbc_options['width'] = $_width;
+	if (strlen($_displayPagesOrPosts = get_option('fbComments_displayPagesOrPosts')) > 1) $fbc_options['displayPagesOrPosts'] = $_displayPagesOrPosts;
+	if (strlen($_containerCss = get_option('fbComments_containerCss')) > 1) $fbc_options['containerCss'] = $_containerCss;
+	if (strlen($_titleCss = get_option('fbComments_titleCss')) > 1) $fbc_options['titleCss'] = $_titleCss;
+	
+		
+	update_option('fbComments', $fbc_options);
+}
+
+// Email the site owner the current XID upon plugin deactivation
+function fbComments_deactivate() {
+	global $fbc_options;
+	
+	$to = get_bloginfo('admin_email');
+	$subject = "[Facebook Comments for WordPress] Your current XID";
+
+	$message = "Thanks for trying out Facebook Comments for WordPress!\n\n" .
+			   "We just thought you'd like to know that your current XID is: {$fbc_options['xid']}.\n\n" .
+			   "This should be saved in your website's database, but in case it gets lost, you'll need this unique key to retrieve your comments should you ever choose to activate this plugin again.\n\n" .
+			   "Have a great day!";
+
+	// Wordwrap the message and strip slashes that may have wrapped quotes
+	$message = stripslashes(wordwrap($message, 70));
+
+	$headers = "From: Facebook Comments for WordPress <$to>\r\n" .
+			   "Reply-To: $to\r\n" .
+			   "X-Mailer: PHP" . phpversion();
+
+	// Send the email notification
+	fbComments_log("Sending XID via email to $to");
+	if (wp_mail($to, $subject, $message, $headers)) {
+		fbComments_log(sprintf('    Sent XID via email to %s', $to));
+	} else {
+		fbComments_log(sprintf('    FAILED to send XID via email to %s', $to));
+	}
+}
+
+// Remove database entries upon the plugin being uninstalled
+function fbComments_uninit() {
+	delete_option('fbComments');
+	delete_option('fbComments_ver');
+}
+
+
+// Generate a random alphanumeric string for the comments XID
+function fbComments_getRandXid($length=15) {
+	$chars = array_merge(range('A', 'Z'), range('a', 'z'), range(0, 9));
+	$rand = '';
+
+	for ($i = 0; $i < $length; $i++) {
+		$rand .= $chars[mt_rand(0, count($chars)-1)];
+	}
+
+	return $rand;
+}
+
+// The application ID and application secret must be set before calling this function
+function fbComments_getFbApi() {
+	global $fbc_options;
+	
+	$fbApiCredentials = array(
+		'appId'	 => $fbc_options['appId'],
+		'secret' => $fbc_options['appSecret']
+	);
+
+	return new Facebook($fbApiCredentials);
+}
+
+// The application ID and application secret must be set before calling this function
+function fbComments_storeAccessToken() {
+	fbComments_log('In ' . __FUNCTION__ . '()');
+	global $fbc_options;
+
+	if (!$fbc_options['accessToken']) {
+		$accessToken = substr(fbComments_getUrl("https://graph.facebook.com/oauth/access_token?type=client_cred&client_id={$fbc_options['appId']}&client_secret={$fbc_options['appSecret']}"), 13);
+		fbComments_log("    got an access token of [$accessToken]");
+		if ($accessToken != '') {
+			fbComments_log("    Storing an access token of $accessToken");
+			$fbc_options['accessToken'] = $accessToken;
+			update_option('fbComments', $fbc_options);
+		} else {
+			fbComments_log('    FAILED to obtain an access token');
+		}
+	}
+}
+
+// sugar for calling wp_remote_get
+function fbComments_getUrl($url) {
+
+	$file_contents = wp_remote_get($url,
+				  $args = array('method' 		=> 'GET',
+								'timeout' 		=> '5',
+								'redirection' 	=> '5',
+								'user-agent' 	=> 'WordPress facebook comments plugin',
+								'blocking'		=> true,
+								'compress'		=> false,
+								'decompress'	=> true,
+								'sslverify'		=> false
+						));
+	// var_dump($file_contents['body']);
+	$file_contents = $file_contents['body'];
+	
+
+	fbComments_log('In ' . __FUNCTION__ . "(url=$url)");
+
+	if (!$file_contents) {
+		fbComments_log('    FAILED to retrieve content via wp_remote_get');
+	}
+
+	return $file_contents;
+}
+
+// Log to the Apache error log (usually located in /var/log/apache2/error_log)
+function fbComments_log($msg) {
+	if (FBCOMMENTS_ERRORS) {
+		error_log('fbComments: ' . $msg);
+	}
+}
+?>
---
+++ facebook-comments-display.php
@@ -0,0 +1,230 @@
+<?php
+	/**********************************
+	 Facebook comments box inclusion
+	 **********************************/
+
+	// Insert Facebook comments manually or into the comments array
+	function facebook_comments($comments='') {
+		global $fbc_options, $wp_query;
+
+		// Return out of function if commenting is closed for this post
+	    if (!comments_open()) {
+			echo "<h2>Comments Closed</h2>";
+	    	return $comments;
+	    }
+
+	    // Return out of function if we're only supposed to display comments on pages OR posts
+	    if (($fbc_options['displayPagesOrPosts'] == 'pages') && (!is_page())) {
+	    	return $comments;
+	    }
+
+	    if (($fbc_options['displayPagesOrPosts'] == 'posts') && (!is_single())) {
+	    	return $comments;
+	    }
+
+		$postId = $wp_query->post->ID;
+	    $xid = $fbc_options['xid'] . "_post$postId";
+	    $postTitle = get_the_title($postId);
+	    $postUrl = get_permalink($postId);
+
+	    // Decide which stylesheet to use
+	    $customStylesheet = fbComments_getStylesheet();
+		
+		// if(empty($fbc_options['appId'])) $fbc_options = get_option('fbComments'); 	
+		// if(empty($fbc_options['appSecret'])) $fbc_options = get_option('fbComments'); 
+		// if(empty($fbc_options['appId'])) $fbc_options['appId'] = get_option('fbComments_appId');
+		// if(empty($fbc_options['appSecret'])) $fbc_options['appSecret'] = get_option('fbComments_appSecret');
+		
+		
+		
+		
+		
+		// Only insert the Facebook comments if both an application ID and an application secret has been set
+		if (!empty($fbc_options['appId']) && !empty($fbc_options['appSecret'])) {
+			// Store our access token if it hasn't already been saved
+			fbComments_storeAccessToken();
+
+			echo "\n<!-- Facebook Comments for WordPress v" . FBCOMMENTS_VER . " by " . FBCOMMENTS_AUTHOR . " (" . FBCOMMENTS_WEBPAGE . ") -->\n
+<a name='facebook-comments'></a>\n";
+
+	    	if ($fbc_options['includeFbJs']) {
+	    		fbComments_includeFbJs();
+			}
+
+			// Print out the JavaScript that will catch new comments in order to update comment counts and send notifications
+			fbComments_printCommentCatchAndNotificationScripts($xid, $postTitle, $postUrl);
+
+        	echo "\n<div id='fbComments' style='{$fbc_options['containerCss']}'>\n";
+
+        	if ($fbc_options['displayTitle']) {
+        		echo "\t<p style='{$fbc_options['titleCss']}'>" . __($fbc_options['title']) . "</p>\n";
+        	}
+
+    		// Print out the JavaScript for calculating the width of the comments box
+    		fbComments_printFbCommentsTag($xid, $postTitle, $postUrl, $customStylesheet);
+
+			// Hide the WordPress commenting form if requested
+			if ($fbc_options['hideWpComments']) {
+			    return array(); // Must return an empty array so foreach() loops in certain themes don't produce errors
+			}
+		} else { // If no application ID or application secret are set, display a message asking the user to set one (if they have permission to do so)
+			fbComments_handleNoAppId();
+		}
+
+		return $comments;
+	}
+
+	/**********************************
+	 Facebook comments box helpers
+	 **********************************/
+
+	function fbComments_getStylesheet() {
+		global $fbc_options;
+
+		if (($fbc_options['hideFbLikeButton']) && ($fbc_options['darkSite'])) {
+	    	return FBCOMMENTS_CSS_HIDELIKEANDDARKSITE . '?' . fbComments_getRandXid();
+	    } elseif ($fbc_options['hideFbLikeButton']) {
+	    	return FBCOMMENTS_CSS_HIDELIKE . '?' . fbComments_getRandXid();
+	    } elseif ($fbc_options['darkSite']) {
+	    	return FBCOMMENTS_CSS_DARKSITE . '?' . fbComments_getRandXid();
+	    } else {
+	    	return FBCOMMENTS_CSS_HIDEFBLINK . '?' . fbComments_getRandXid();
+	    }
+	}
+
+	function fbComments_handleNoAppId() {
+		global $fbc_options;
+
+		get_currentuserinfo(); // Get user info to see if the currently logged in user (if any) has the 'manage_options' capability
+
+		if (current_user_can('manage_options')) {
+		    $fbc_optionsPage = admin_url('options-general.php?page=facebook-comments');
+
+		    echo "\n<!-- Facebook Comments for WordPress v" . FBCOMMENTS_VER . " by " . FBCOMMENTS_AUTHOR . " (" . FBCOMMENTS_WEBPAGE . ") -->
+
+<div style='{$fbc_options['containerCss']}'>\n";
+
+			if ($fbc_options['displayTitle']) {
+				echo "\t<p style='{$fbc_options['titleCss']}'>" . __($fbc_options['title']) . "</p>\n";
+			}
+
+			echo "\t<div style='background-color: #ffebe8; border: 1px solid #c00; padding: 7px; font-weight: bold; border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; -khtml-border-radius: 5px;'>" . __("Your Facebook comments would normally appear here, but you haven't set a valid application ID or application secret yet. <a href='$fbc_optionsPage' style='color: #c00;'>Go set them now</a>.") . "</div>
+</div>\n";
+		}
+	}
+
+	function fbComments_includeFbJs() {
+		global $fbc_options;
+
+		// Decide whether or not to include the Facebook JavaScript library the old way or the new way
+	    if ($fbc_options['includeFbJsOldWay']) {
+	    	echo "\n<div id='fb-root'></div>
+<script type='text/javascript'>
+    window.fbAsyncInit = function() {
+    	FB.init({
+    		appId: '{$fbc_options['appId']}',
+    		status: true,
+    		cookie: true,
+    		xfbml: true
+    	});
+    };
+
+    (function() {
+    	var e = document.createElement('script'); e.async = true;
+    	e.src = document.location.protocol + '//connect.facebook.net/{$fbc_options['language']}/all.js';
+    	document.getElementById('fb-root').appendChild(e);
+    }());
+</script>\n";
+	    } else {
+	    	echo "\n<div id='fb-root'></div>
+<script src='http://connect.facebook.net/{$fbc_options['language']}/all.js#appId={$fbc_options['appId']}&amp;xfbml=1' type='text/javascript'></script>\n";
+		}
+	}
+
+	function fbComments_printCommentCatchAndNotificationScripts($xid, $postTitle, $postUrl) {
+		global $fbc_options;
+
+		echo "
+<script type='text/javascript'>
+	var addedComment = function(response) {
+		//console.log('fbComments: Caught added comment');
+		//console.log('fbComments:     Making AJAX call to update Facebook comment count');
+		$.post('" . FBCOMMENTS_PATH . "facebook-comments-ajax.php', { fn: 'addComment', xid: '$xid' }, function(resp) {
+			if (resp === 'true') {
+				//console.log('fbComments:     Updated and cached Facebook comment count for post with xid=$xid');
+			} else {
+				//console.log('fbComments:     FAILED to update Facebook comment count for post with xid=$xid');
+			}
+		});\n";
+
+		if ($fbc_options['notify']) {
+			echo "
+		//console.log('fbComments:     Making AJAX call to send email notification');
+		$.post('" . FBCOMMENTS_PATH . "facebook-comments-ajax.php', { fn: 'sendNotification', xid: '$xid', postTitle: '$postTitle', postUrl: '$postUrl' }, function(resp) {
+			if (resp === 'true') {
+				//console.log('fbComments:     Sent email notification');
+			} else {
+				//console.log('fbComments:     FAILED to send email notification');
+			}
+		});";
+		}
+
+		echo "
+	};
+
+	FB.Event.subscribe('comments.add', addedComment);
+</script>\n";
+	}
+
+	function fbComments_printFbCommentsTag($xid, $postTitle, $postUrl, $customStylesheet) {
+		global $fbc_options;
+		// Since the 'publish_feed' option defaults to true, we need to pass it an explicit false if it's turned off
+		$publishToWall = ($fbc_options['publishToWall']) ? 'true' : 'false';
+		// if ($fbc_options['newUser'] == 1) {
+			// echo "\t<fb:comments href='$postUrl' ",
+				// "numposts='{$fbc_options['numPosts']}' ",
+				// "width='{$fbc_options['width']}' ",
+				// "publish_feed='$publishToWall' ",
+				// "</fb:comments>";
+		// }
+		if ($fbc_options['v1plusv2'] == 1) {
+			$fbc_options['hideFbLikeButton'] = true;
+			update_option('fbComments', $fbc_options);
+			echo "\t<fb:comments xid='$xid' href='$postUrl' ",
+				"numposts='{$fbc_options['numPosts']}' ",
+				"width='{$fbc_options['width']}' ",
+				"publish_feed='$publishToWall' ",
+				"migrated='1'></fb:comments>";
+				
+			echo "\t<fb:comments xid='$xid' ",
+				"numposts='{$fbc_options['numPosts']}' ",
+				"width='{$fbc_options['width']}' ",
+				"simple='{$fbc_options['noBox']}' ",
+				"publish_feed='$publishToWall' ",
+				"reverse='{$fbc_options['reverseOrder']}' ",
+				"css='$customStylesheet' ",
+				"title='$postTitle' ",
+				"url='$postUrl' ",
+				"notify='true'></fb:comments>";
+		} else if ($fbc_options['newFBC'] == 1) {
+			// $xid = urlencode($xid);
+			echo "\t<fb:comments xid='$xid' ",
+				"numposts='{$fbc_options['numPosts']}' ",
+				"width='{$fbc_options['width']}' ",
+				"publish_feed='$publishToWall' ",
+				"migrated='1'></fb:comments>";
+		
+		} else {
+			echo "\t<fb:comments xid='$xid' ",
+				"numposts='{$fbc_options['numPosts']}' ",
+				"width='{$fbc_options['width']}' ",
+				"simple='{$fbc_options['noBox']}' ",
+				"publish_feed='$publishToWall' ",
+				"reverse='{$fbc_options['reverseOrder']}' ",
+				"css='$customStylesheet' ",
+				"title='$postTitle' ",
+				"url='$postUrl' ",
+				"notify='true'></fb:comments>";
+		}
+echo "</div>\n";
+	}
